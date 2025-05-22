044daed7c4e6d76864c3a8e7f1fc5d702fd6bca1
pvntheraxxx <pvntheraxxx@gmail.com>
2025-05-22 14:41:24 +0300

    create a welcome presentation swiper and added types

--- app-env.d.ts
+++ app-env.d.ts
@@ -1,2 +1,8 @@
 // @ts-ignore
 /// <reference types="nativewind/types" />
+
+// app-env.d.ts
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,8 +1,14 @@
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, View, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { router } from 'expo-router';
+import Swiper from 'react-native-swiper';
+import { useRef, useState } from 'react';
+import { onboarding } from 'constants/index';
 
 const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
@@ -12,6 +18,25 @@ const Onboarding = () => {
         className="flex w-full items-end justify-end p-5">
         <Text className="text-md font-JakartaBold text-black">Skip</Text>
       </TouchableOpacity>
+
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={<View className="mx-1 h-[4px] w-[32px] rounded-full bg-[#E2E8F0]" />}
+        activeDot={<View className="mx-1 h-[4px] w-[32px] rounded-full bg-[#0286FF]" />}
+        onIndexChanged={(index) => setActiveIndex(index)}>
+        {onboarding.map((item) => (
+          <View key={item.id} className="flex items-center justify-center p-5">
+            <Image source={item.image} className="h-[300px] w-full" resizeMode="contain" />
+            <View className="mt-10 flex w-full flex-row items-center justify-center">
+              <Text className="mx-10 text-center text-3xl font-bold text-black">{item.title}</Text>
+            </View>
+            <Text className="mx-10 mt-3 text-center font-JakartaSemiBold text-lg text-[#858585]">
+              {item.description}
+            </Text>
+          </View>
+        ))}
+      </Swiper>
     </SafeAreaView>
   );
 };
