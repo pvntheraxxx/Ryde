6eb611c55a34453d6d53f37c638b797d33ca6d77
pvntheraxxx <pvntheraxxx@gmail.com>
2025-05-22 10:11:47 +0300

    added assets and some changes in app folder structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,10 @@ import { Stack } from 'expo-router';
 import '../global.css';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
 import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function Home() {
+const Home = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-2xl font-bold text-rose-600">✅ NativeWind работает!</Text>
-    </View>
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
   );
-}
+};
+
+export default Home;
