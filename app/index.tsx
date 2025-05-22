79c131aaa7922c9c5935fd6f1dea493551489ff1
pvntheraxxx <pvntheraxxx@gmail.com>
2025-05-22 08:30:35 +0300

    d*mn... I must be stronger to get the correct structure for my project

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const Home = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-2xl font-bold text-white">✅ NativeWind работает!</Text>
+    </View>
+  );
+};
+
+export default Home;
