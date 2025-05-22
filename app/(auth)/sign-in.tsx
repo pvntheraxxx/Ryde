993e03af404384485b74d2283923a4a667db1460
pvntheraxxx <pvntheraxxx@gmail.com>
2025-05-22 19:53:17 +0300

    solved some little bugs

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,10 +1,11 @@
 import { images, icons } from '@/constants';
-import { Text, ScrollView, View, Image } from 'react-native';
+import { Text, ScrollView, View, Image, KeyboardAvoidingView, Platform } from 'react-native';
 import InputField from '@/components/InputField';
 import CustomButton from '@/components/CustomButton';
 import OAuth from '@/components/OAuth';
 import { useState } from 'react';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const SignIn = () => {
   const [form, setForm] = useState({
@@ -15,51 +16,58 @@ const SignIn = () => {
   const onSignInPress = async () => {};
 
   return (
-    <ScrollView className="flex-1 bg-white">
-      <View className="flex-1 bg-white">
-        <View className="relative h-[250px] w-full">
-          <Image source={images.signUpCar} className="z-0 h-[250px] w-full" />
-          <Text className="font-JakartaSemiBold text-2xl text-black">Welcome 👋</Text>
-        </View>
-        <View className="p-5">
-          <InputField
-            label="Email"
-            placeholder="Enter your email"
-            icon={icons.email}
-            value={form.email}
-            onChangeText={(value) =>
-              setForm({
-                ...form,
-                email: value,
-              })
-            }
-          />
-          <InputField
-            label="Password"
-            placeholder="Enter your password"
-            icon={icons.lock}
-            secureTextEntry={true}
-            value={form.password}
-            onChangeText={(value) =>
-              setForm({
-                ...form,
-                password: value,
-              })
-            }
-          />
+    <SafeAreaView className="flex-1 bg-white">
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        className="flex-1"
+        keyboardVerticalOffset={20}>
+        <ScrollView contentContainerStyle={{ paddingBottom: 40 }}>
+          <View className="flex-1 bg-white">
+            <View className="relative h-[250px] w-full">
+              <Image source={images.signUpCar} className="z-0 h-[250px] w-full" />
+              <Text className="font-JakartaSemiBold text-2xl text-black">Welcome 👋</Text>
+            </View>
+            <View className="p-5">
+              <InputField
+                label="Email"
+                placeholder="Enter your email"
+                icon={icons.email}
+                value={form.email}
+                onChangeText={(value) =>
+                  setForm({
+                    ...form,
+                    email: value,
+                  })
+                }
+              />
+              <InputField
+                label="Password"
+                placeholder="Enter your password"
+                icon={icons.lock}
+                secureTextEntry={true}
+                value={form.password}
+                onChangeText={(value) =>
+                  setForm({
+                    ...form,
+                    password: value,
+                  })
+                }
+              />
 
-          <CustomButton title="Sign In" onPress={onSignInPress} className="mt-6" />
+              <CustomButton title="Sign In" onPress={onSignInPress} className="mt-6" />
 
-          <OAuth />
+              <OAuth />
 
-          <Link href="/sign-up" className="mt-10 text-center text-lg text-general-200">
-            <Text>Do not have an account?</Text>
-            <Text className="text-primary-500">Sign Up</Text>
-          </Link>
-        </View>
-        {/* Verification Modal */}
-      </View>
-    </ScrollView>
+              <Link href="/sign-up" className="mt-10 text-center text-lg text-general-200">
+                <Text>Do not have an account?</Text>
+                <Text className="text-primary-500">Sign Up</Text>
+              </Link>
+            </View>
+            {/* Verification Modal */}
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,10 +1,11 @@
 import { images, icons } from '@/constants';
-import { Text, ScrollView, View, Image } from 'react-native';
+import { Text, ScrollView, View, Image, KeyboardAvoidingView, Platform } from 'react-native';
 import InputField from '@/components/InputField';
 import CustomButton from '@/components/CustomButton';
 import OAuth from '@/components/OAuth';
 import { useState } from 'react';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const SignUp = () => {
   const [form, setForm] = useState({
@@ -16,63 +17,70 @@ const SignUp = () => {
   const onSignUpPress = async () => {};
 
   return (
-    <ScrollView className="flex-1 bg-white">
-      <View className="flex-1 bg-white">
-        <View className="relative h-[250px] w-full">
-          <Image source={images.signUpCar} className="z-0 h-[250px] w-full" />
-        </View>
-        <Text className="font-JakartaSemiBold text-2xl text-black">Create Your Account</Text>
-        <View className="p-5">
-          <InputField
-            label="Name"
-            placeholder="Enter your name"
-            icon={icons.person}
-            value={form.name}
-            onChangeText={(value) =>
-              setForm({
-                ...form,
-                name: value,
-              })
-            }
-          />
-          <InputField
-            label="Email"
-            placeholder="Enter your email"
-            icon={icons.email}
-            value={form.email}
-            onChangeText={(value) =>
-              setForm({
-                ...form,
-                email: value,
-              })
-            }
-          />
-          <InputField
-            label="Password"
-            placeholder="Enter your password"
-            icon={icons.lock}
-            secureTextEntry={true}
-            value={form.password}
-            onChangeText={(value) =>
-              setForm({
-                ...form,
-                password: value,
-              })
-            }
-          />
+    <SafeAreaView className="flex-1 bg-white">
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        className="flex-1"
+        keyboardVerticalOffset={20}>
+        <ScrollView contentContainerStyle={{ paddingBottom: 40 }}>
+          <View className="flex-1 bg-white">
+            <View className="relative h-[250px] w-full">
+              <Image source={images.signUpCar} className="z-0 h-[250px] w-full" />
+            </View>
+            <Text className="font-JakartaSemiBold text-2xl text-black">Create Your Account</Text>
+            <View className="p-5">
+              <InputField
+                label="Name"
+                placeholder="Enter your name"
+                icon={icons.person}
+                value={form.name}
+                onChangeText={(value) =>
+                  setForm({
+                    ...form,
+                    name: value,
+                  })
+                }
+              />
+              <InputField
+                label="Email"
+                placeholder="Enter your email"
+                icon={icons.email}
+                value={form.email}
+                onChangeText={(value) =>
+                  setForm({
+                    ...form,
+                    email: value,
+                  })
+                }
+              />
+              <InputField
+                label="Password"
+                placeholder="Enter your password"
+                icon={icons.lock}
+                secureTextEntry={true}
+                value={form.password}
+                onChangeText={(value) =>
+                  setForm({
+                    ...form,
+                    password: value,
+                  })
+                }
+              />
 
-          <CustomButton title="Sign Up" onPress={onSignUpPress} className="mt-6" />
+              <CustomButton title="Sign Up" onPress={onSignUpPress} className="mt-6" />
 
-          <OAuth />
+              <OAuth />
 
-          <Link href="/sign-in" className="mt-10 text-center text-lg text-general-200">
-            <Text>Already have an account?{''}</Text>
-            <Text className="text-primary-500">Log In</Text>
-          </Link>
-        </View>
-        {/* Verification Modal */}
-      </View>
-    </ScrollView>
+              <Link href="/sign-in" className="mt-10 text-center text-lg text-general-200">
+                <Text>Already have an account?{''}</Text>
+                <Text className="text-primary-500">Log In</Text>
+              </Link>
+            </View>
+            {/* Verification Modal */}
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
