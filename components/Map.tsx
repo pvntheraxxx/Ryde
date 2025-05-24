34ab0994ab4dd93b00312f176c79273f0a1b82dc
pvntheraxxx <pvntheraxxx@gmail.com>
2025-05-24 19:26:46 +0300

    added Google Api

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,10 +1,20 @@
 import { View, Text } from 'react-native';
+import MapView, { PROVIDER_DEFAULT } from 'react-native-maps';
 
 const Map = () => {
+  const region = {};
   return (
-    <View>
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="h-full w-full rounded-2xl"
+      tintColor="black"
+      // showsPointsOfInterest={false}
+      // initialRegion={region}
+      showsUserLocation={true}
+      userInterfaceStyle="light"
+      mapType="mutedStandard">
       <Text>Map</Text>
-    </View>
+    </MapView>
   );
 };
 
